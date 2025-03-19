784c5eea7f73082d6c1bb13d75e34453c1aba17e
みねちゃん <mineuchimaru@gmail.com>
2025-03-19 17:42:59 +0900

    Update settings.py for template and static file settings

--- juncbox/settings.py
+++ juncbox/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'juncbox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'music/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
