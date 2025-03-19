f904513c0aa1ec89bd99a2b5c274ca7435dcb0a4
みねちゃん <mineuchimaru@gmail.com>
2025-03-19 13:02:28 +0900

    Update email settings for Render

--- juncbox/settings.py
+++ juncbox/settings.py
@@ -121,9 +121,6 @@ AWS_QUERYSTRING_AUTH = False
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # ローカルでコンソール出力
-DEFAULT_FROM_EMAIL = 'junctifyinfo@gmail.com'  # 送信元メールアドレス
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
